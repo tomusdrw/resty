faa8736ac999f90451c6c2c22b67def7946d73e4
Tomasz Drwięga <tomusdrw@gmail.com>
2017-09-12 01:55:37 +0200

    Clean up advanced example.

--- examples/api.rs
+++ examples/api.rs
@@ -7,60 +7,92 @@ extern crate serde_derive;
 use std::sync::RwLock;
 use futures::Future;
 
+fn main() {
+    let mut v1 = resty::Router::new();
+    v1.add("/products", Products {
+        products: RwLock::new(vec![
+            Product { id: 0, name: "Bread".into() },
+            Product { id: 1, name: "Butter".into() },
+        ]),
+    }.into());
+
+    let mut server = resty::Router::new();
+    // Compose routers to form the API
+    server.add("/v1", v1);
+    server.post("/test", |request| {
+        request.json().map(|mut product: Product| {
+            product.id += 1;
+            product
+        })
+    });
+
+    println!("{}", server.routes());
+
+    let listening = server.bind("localhost:3000").unwrap();
+    listening.wait()
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+struct Product {
+    pub id: usize,
+    pub name: String,
+}
+
 #[derive(Default)]
 struct Products {
-    calls: RwLock<Vec<Call>>,
+    products: RwLock<Vec<Product>>,
 }
 
 impl Products {
-    pub fn list(&self) -> Result<Vec<Call>, resty::Error> {
-        Ok(self.calls.read().unwrap().clone())
+    pub fn list(&self) -> Result<Vec<Product>, resty::Error> {
+        Ok(self.products.read().unwrap().clone())
     }
 
-    pub fn single(&self, id: usize) -> Result<Call, resty::Error> {
-        let calls = self.calls.read().unwrap();
-        if id < calls.len() {
-            Ok(calls[id].clone())
+    pub fn single(&self, id: usize) -> Result<Product, resty::Error> {
+        let products = self.products.read().unwrap();
+        if id < products.len() {
+            Ok(products[id].clone())
         } else {
             Err(resty::Error::not_found(""))
         }
     }
 
-    pub fn add(&self, call: Call) -> Result<Call, resty::Error> {
-        self.calls.write().unwrap().push(call.clone());
-        Ok(call)
+    pub fn add(&self, product: Product) -> Result<Product, resty::Error> {
+        self.products.write().unwrap().push(product.clone());
+        Ok(product)
     }
 
-    pub fn update(&self, id: usize, call: Call) -> Result<Call, resty::Error> {
-        let mut calls = self.calls.write().unwrap();
-        if id < calls.len() {
-            calls[id] = call.clone();
-            Ok(call)
+    pub fn update(&self, id: usize, product: Product) -> Result<Product, resty::Error> {
+        let mut products = self.products.write().unwrap();
+        if id < products.len() {
+            products[id] = product.clone();
+            Ok(product)
         } else {
             Err(resty::Error::not_found(""))
         }
     }
 }
 
-// TODO [ToDr] Derive this implementatio
+// TODO [ToDr] Derive this implementation
 impl Into<resty::Router> for Products {
     fn into(self) -> resty::Router {
-        let mut router = resty::Router::new();
         let self_ = ::std::sync::Arc::new(self);
-        let a = self_.clone();
+        let mut router = resty::Router::new();
+
         // no params
+        let a = self_.clone();
         router.get("/", move |_request| {
             a.list()
         });
 
-        let a = self_.clone();
         // dynamic params
+        let a = self_.clone();
         router.get("/{id}", move |request| {
             a.single(request.params().get_usize("id")?)
         });
 
-        let a = self_.clone();
         // static params
+        let a = self_.clone();
         router.get(url!(/test/{id:usize}), move |request| {
             a.single(request.params().id)
         });
@@ -69,45 +101,20 @@ impl Into<resty::Router> for Products {
         router.put(url!(/{id:usize}), move |request| {
             let a = a.clone();
             let id = request.params().id;
-            request.json().map_err(Into::into).and_then(move |call: Call| {
-                a.update(id, call)
+            request.json().map_err(Into::into).and_then(move |product| {
+                a.update(id, product)
             })
         });
 
-        let a = self_.clone();
         // post request
+        let a = self_.clone();
         router.post("/", move |request| {
             let a = a.clone();
-            request.json().map_err(Into::into).and_then(move |call: Call| {
-                a.add(call)
+            request.json().map_err(Into::into).and_then(move |product| {
+                a.add(product)
             })
         });
 
         router
     }
-}
-
-#[derive(Deserialize, Serialize, Clone)]
-struct Call {
-    pub test: u64,
-}
-
-fn main() {
-    let mut v1 = resty::Router::new();
-    v1.add("/products", Products {
-        calls: RwLock::new(vec![Call { test: 1 }, Call { test: 2}]),
-    }.into());
-
-    let mut server = resty::Router::new();
-    server.add("/v1", v1);
-    server.post("/test", |request| {
-        request.json().map(|mut call: Call| {
-            call.test += 1;
-            call
-        })
-    });
-
-    println!("{}", server.routes());
-    let listening = server.bind("localhost:3000").unwrap();
-    listening.wait()
 }
